6f21426fd6ebb188f0a1b34246afcccf811f8eed
Aidan Leary <learyaidan@gmail.com>
2025-08-03 20:39:22 -0400

    working implementation of adc dma transfers, with swapping buffers

---
+++ opamp-adc-test/src/adc_dma.rs
@@ -0,0 +1,86 @@
+// use crate::Buffer;
+use crate::BUFFER_SIZE;
+
+use stm32g4xx_hal as hal;
+
+use stm32g4xx_hal::{adc::{Disabled, DMA, Adc}, dma::{channel::C, traits::{Channel, TargetAddress}}};
+
+use core::{mem, sync::atomic::fence};
+
+pub struct AdcDmaTransfer<A, D> 
+where 
+    A: hal::adc::Instance,
+    D: hal::dma::channel::Instance,
+{
+    adc: Option<Adc<A, DMA>>, 
+    channel: C<D, 0>, 
+    buffer: &'static mut [u16; BUFFER_SIZE]
+}
+
+impl<A, D> AdcDmaTransfer<A, D> 
+where 
+    A: hal::adc::Instance,
+    D: hal::dma::channel::Instance
+{
+    /// Configure and start the first transfer.
+    pub fn init(mut adc: Adc<A, Disabled>, mut channel: C<D, 0>, buffer: &'static mut [u16; BUFFER_SIZE]) -> Self
+    {
+        adc.set_continuous(hal::adc::config::Continuous::Continuous);
+        let mut adc_dma = adc.enable_dma(hal::adc::config::Dma::Single);
+
+        channel.disable(); 
+        fence(core::sync::atomic::Ordering::SeqCst);
+
+        unsafe 
+        {
+            channel.set_peripheral_address(adc_dma.address());
+            channel.set_memory_address(buffer.as_ptr() as u32);
+            channel.set_number_of_transfers(BUFFER_SIZE as u16);
+            channel.set_priority(stm32g4xx_hal::dma::config::Priority::Medium);
+            channel.set_direction(stm32g4xx_hal::dma::DmaDirection::PeripheralToMemory);
+            channel.set_circular_buffer(false);
+            channel.set_peripheral_increment(false);
+            channel.set_memory_increment(true);
+            channel.set_peripheral_size(1);
+            channel.set_memory_size(1);
+            channel.set_half_transfer_interrupt_enable(false);
+            channel.set_transfer_error_interrupt_enable(false);
+            channel.set_transfer_complete_interrupt_enable(false);
+            channel.set_request_line(5);
+            fence(core::sync::atomic::Ordering::SeqCst);
+            channel.enable();
+        }
+        adc_dma.start_conversion();
+
+        Self {adc: Some(adc_dma), channel: channel, buffer: buffer}
+    }
+
+    /// Wait for the current transfer to end, swap the current buffer with a new one, and restart
+    /// the transfer.
+    pub fn wait_and_swap_buffers(&mut self, buffer: &'static mut [u16; BUFFER_SIZE]) -> &'static mut [u16; BUFFER_SIZE]
+    {
+
+        while !self.channel.get_transfer_complete_flag() 
+        {
+
+        }
+        self.channel.clear_interrupts();
+
+        let mut adc = self.adc.take().expect("");
+        
+        self.channel.disable();
+        fence(core::sync::atomic::Ordering::SeqCst);
+
+        unsafe 
+        {
+            self.channel.set_memory_address(buffer.as_ptr() as u32);
+            self.channel.set_number_of_transfers(BUFFER_SIZE as u16);
+            fence(core::sync::atomic::Ordering::SeqCst);
+            self.channel.enable();
+        }
+        adc.start_conversion();
+        self.adc = Some(adc);
+
+        mem::replace(&mut self.buffer, buffer)
+    }
+}
--- opamp-adc-test/src/main.rs
+++ opamp-adc-test/src/main.rs
@@ -1,28 +1,25 @@
 #![no_main] 
 #![no_std]
 
-use stm32g4xx_hal::{
-    self as hal, adc::{config::{self, Continuous, SampleTime, Sequence}, Adc, AdcClaim, AdcCommonExt, DMA}, cortex_m::{self, asm::delay}, dma::{self, channel::{DMAExt, C}, config::DmaConfig, transfer::MutTransfer, PeripheralToMemory, Transfer, TransferExt}, gpio::{self, *}, opamp::Gain, pac::{GPIOB}, prelude::*, pwr, rcc::{self, RccExt}, stm32::{self, interrupt, Interrupt}, stm32g4::{self, Periph}
+use stm32g4xx_hal as hal;
+
+use hal::{stm32,
+    adc::{config::{self, SampleTime, Sequence}, AdcClaim, AdcCommonExt}, cortex_m::{self}, dma::{channel::DMAExt}, opamp::{OpampEx, Gain, IntoPga}, pwr::{PwrExt}, rcc::{self, RccExt}, 
+    gpio::GpioExt, delay::SYSTDelayExt,
 };
 
 use cortex_m_rt::{entry};
-use core::cell::RefCell;
-use cortex_m::{asm::wfi, interrupt::Mutex};
 use panic_probe as _;
 use defmt_rtt as _;
 use defmt::{info, println};
 
 mod amplifier;
-use crate::amplifier::{OpampExt, OpampGainDB}; 
+mod adc_dma;
+use crate::{adc_dma::AdcDmaTransfer, amplifier::{OpampExt, OpampGainDB}}; 
 
-type Adc1Transfer = Transfer<C<Periph<crate::hal::pac::dma1::RegisterBlock, 1073872896>, 0>, Adc<Periph<hal::pac::adc1::RegisterBlock, 1342177280>, DMA>, PeripheralToMemory, &'static mut [u16], MutTransfer>;
+
+const BUFFER_SIZE: usize = 100;
 type Buffer = [u16; BUFFER_SIZE];
-
-const BUFFER_SIZE: usize = 7000;
-
-static TRANSFER: Mutex<RefCell<Option<Adc1Transfer>>> = Mutex::new(RefCell::new(None));
-static mut BUFFER1: Buffer = [0; BUFFER_SIZE];
-static mut BUFFER2: Buffer = [0; BUFFER_SIZE];
 
 #[entry]
 fn main() -> ! 
@@ -41,105 +38,52 @@ fn main() -> !
 
     // configure led
     let gpiob = dp.GPIOB.split(&mut rcc);
-    // let gpioc = dp.GPIOC.split(&mut rcc);
     let mut led = gpiob.pb8.into_push_pull_output();
 
     let gpioa = dp.GPIOA.split(&mut rcc);
+    let pa0 = gpioa.pa0.into_analog();
     let pa1 = gpioa.pa1.into_analog();
-    let pa5 = gpioa.pa5.into_analog();
     let pa3 = gpioa.pa3.into_analog();
-    let pa7 = gpioa.pa7.into_analog();
 
     // dma
     let channels = dp.DMA1.split(&rcc);
-    let dma_config = DmaConfig::default()
-        .transfer_complete_interrupt(false)
-        .circular_buffer(false)
-        .memory_increment(true);
+
+    let ch1 = channels.ch1;
 
     // configure opamps
-    let (opamp1, opamp2, _opamp3) = dp.OPAMP.split(&mut rcc);
+    let (opamp1, _opamp2, _opamp3) = dp.OPAMP.split(&mut rcc);
     let mut pga1 = opamp1.pga_external_bias(pa1, pa3, Gain::Gain2);
-    let mut pga2 = opamp2.pga_external_bias(pa7, pa5, Gain::Gain2);
     pga1.set_gain(OpampGainDB::Gain6);
-    pga2.set_gain(OpampGainDB::Gain6);
 
     // configure adcs
     let clk_cfg = config::ClockMode::AdcKerCk { prescaler: (config::Prescaler::Div_1), src: (config::ClockSource::PllP) };
     let adc12_common = dp.ADC12_COMMON.claim(clk_cfg, &mut rcc);
-    let mut adc1 = adc12_common.claim(dp.ADC1, &mut delay);
-    let mut adc2 = adc12_common.claim(dp.ADC2, &mut delay);
-    adc1.set_continuous(Continuous::Continuous);
-    adc1.reset_sequence(); 
-    adc1.configure_channel(&pga1, Sequence::One, SampleTime::Cycles_2_5);
+    let mut adc = adc12_common.claim(dp.ADC1, &mut delay);
 
-    adc2.set_continuous(Continuous::Continuous);
-    adc2.reset_sequence();
-    adc2.configure_channel(&pga2, Sequence::One, SampleTime::Cycles_2_5);
+    adc.reset_sequence(); 
+    adc.configure_channel(&pa0, Sequence::One, SampleTime::Cycles_2_5);
 
-    // let mut first_buffer = cortex_m::singleton!(: [u16; 10000] = [0; 10000]).unwrap();
+    let mut aux_buffer = cortex_m::singleton!(BUF2: Buffer = [0; BUFFER_SIZE]).unwrap();
+    let first_buffer = cortex_m::singleton!(BUF1 : Buffer = [0; BUFFER_SIZE]).unwrap();
 
-    // cortex_m::interrupt::free(|cs| *TRANSFER.borrow(cs).borrow_mut() = Some(transfer));
+    let mut transfer = AdcDmaTransfer::init(adc, ch1, first_buffer);
 
-    let mut ch1 = channels.ch1;
+    loop {
+        // wait for next buffer
+        let buffer = transfer.wait_and_swap_buffers(aux_buffer);
 
-    let mut buf1_active = true;
+        // process
+        let max_val = max(buffer);
+        let avg_val = avg(buffer);
+        println!("max value: {}", max_val);
+        println!("avg value: {}", avg_val);
 
-    // MAIN LOOP 
-    loop 
-    {
-
-        let mut transfer = match buf1_active {
-            true => {
-                unsafe {
-                    ch1.into_peripheral_to_memory_transfer(
-                        adc1.enable_dma(config::Dma::Single),
-                        &mut BUFFER1[..],
-                        dma_config,
-                    )
-                } 
-            },
-            false => {
-                unsafe {
-                    ch1.into_peripheral_to_memory_transfer(
-                        adc1.enable_dma(config::Dma::Single),
-                        &mut BUFFER2[..],
-                        dma_config,
-                    )
-                }
-            },
-        };
-
-        transfer.start(|adc| adc.start_conversion());
-
-        info!("waiting for transfer");
-        while !transfer.get_transfer_complete_flag() { } 
-        info!("done with transfer");
-
-        // let max = transfer.peek_buffer(|buf, size| max(buf));
-        // println!("max: {}", max);
-
-        let (ch1_, adc1_, _) = transfer.free();
-        ch1 = ch1_; 
-        adc1 = adc1_.disable();
-
-        let max = match buf1_active {
-            true => unsafe {max(&BUFFER1[..])}, 
-            false => unsafe {max(&BUFFER2[..])} 
-        };
-        println!("max: {}", max);
-
-        // let sample1 = adc1.convert(&pga1, SampleTime::Cycles_640_5);
-        // let sample2 = adc2.convert(&pga2, SampleTime::Cycles_640_5);
-        // info!{"adc1 sample: {}", sample1};
-        // info!{"adc2 sample: {}", sample2};
-        led.toggle();
-        delay.delay_ms(100);
-        buf1_active = !buf1_active;
+        // assign auxillary buffer
+        aux_buffer = buffer;
     }
 }
 
-fn max(list: &'static [u16]) -> u16
+fn max(list: &[u16]) -> u16
 {
     let mut max: u16 = 0;
     for i in 0..list.len() {
@@ -148,6 +92,16 @@ fn max(list: &'static [u16]) -> u16
         }
     }
     max
+}
+
+fn avg(list: &[u16]) -> u16 
+{
+    let mut sum: u32 = 0;
+    for i in 0..list.len() 
+    {
+        sum += list[i] as u32;
+    }
+    ((sum as f32) / (list.len() as f32)) as u16
 }
 
 /// Configures clocks. 
@@ -167,12 +121,6 @@ fn config_clks(rcc: stm32::RCC, pwr: stm32::PWR) -> rcc::Rcc
     let clk_cfg = clk_cfg.pll_cfg(pll_cfg);             
 
     // apply config
-    let pwr = pwr.constrain().vos(pwr::VoltageScale::Range1{ enable_boost: true }).freeze();
+    let pwr = pwr.constrain().vos(hal::pwr::VoltageScale::Range1{ enable_boost: true }).freeze();
     rcc.constrain().freeze(clk_cfg, pwr)
-}
-
-#[interrupt] 
-fn DMA1_CH1()
-{
-
 }
